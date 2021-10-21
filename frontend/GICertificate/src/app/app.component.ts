b68b216a9446977378cd696527cbdbffcbd2611b
kishore-shiva <kishoreshiva.s@gmail.com>
2021-10-21 22:58:26 +0530

    Kishore test

--- frontend/GICertificate/src/app/app.component.ts
+++ frontend/GICertificate/src/app/app.component.ts
@@ -4,7 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+}) 
+
 export class AppComponent {
   title = 'GICertificate';
+  buyer = true;
+  showBuyer = false;
+  showSeller = false;
+
+  clickBuyer(){
+    this.showBuyer = true;
+  }
+
+  ngOnInit(){
+  }
 }
+
+
