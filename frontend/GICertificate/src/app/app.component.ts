23ca4901fb2f67da8dde524c30280adea2e26a51
blackbug-official <68537298+blackbug-official@users.noreply.github.com>
2021-10-22 10:45:10 +0530

    [Harish] add sign in and sign up

--- frontend/GICertificate/src/app/app.component.ts
+++ frontend/GICertificate/src/app/app.component.ts
@@ -8,16 +8,70 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'GICertificate';
-  buyer = true;
-  showBuyer = false;
-  showSeller = false;
+  
+  BuyerButton = true;
+  SellerButton = true;
 
-  clickBuyer(){
-    this.showBuyer = true;
+  isSellerButtonClicked = false;
+  isBuyerButtonClicked = false;
+
+  sellerSignUpButton = false;
+  sellerSignInButton = false;
+
+  buyerSignUpButton = false;
+  buyerSignInButton = false;
+
+  buyerSignUpForm = false;
+  sellerSignUpForm = false;
+
+  buyerSignInForm = false;
+  sellerSignInForm = false;
+
+  sellerAddProducts = false;
+
+  clickBuyerButton(){
+    this.SellerButton = false;
+    this.BuyerButton = false;
+    this.isBuyerButtonClicked = true;
+    this.buyerSignUpButton = true;
+    this.buyerSignInButton = true;
   }
 
-  ngOnInit(){
+  clickSellerButton(){
+    this.SellerButton = false;
+    this.BuyerButton = false;
+    this.isSellerButtonClicked = true;
+    this.sellerSignUpButton = true;
+    this.sellerSignInButton = true;
   }
+
+  clickSellerSignUp(){
+    this.sellerSignUpButton = false;
+    this.sellerSignInButton = false;
+    this.sellerSignUpForm = true;
+  }
+
+  clickSellerSignIn(){
+    this.sellerSignUpButton = false;
+    this.sellerSignInButton = false;
+    this.sellerSignInForm = true;
+  }
+
+  clickBuyerSignUp(){
+    this.buyerSignUpButton = false;
+    this.buyerSignInButton = false;
+    this.buyerSignUpForm = true;
+  }
+
+  clickBuyerSignIn(){
+    this.buyerSignUpButton = false;
+    this.buyerSignInButton = false;
+    this.buyerSignInForm = true;
+  }
+  
+
+  ngOnInit(){}
+
 }
 
 
